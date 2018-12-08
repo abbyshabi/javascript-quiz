d587afbda1e7209c63443e0dc744e24fc64343a6
abby-shabi <abbyshabi@gmail.com>
2018-12-08 13:36:16 +0300

    declared javascript variables

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function compute() {
+	var Q1 = document.getElementsByName('question1');
+	var Q2 = document.getElementsByName('question2');
+	var Q3 = document.getElementsByName('question3');
+	var Q4 = document.getElementsByName('question4');
+	var score = 0;
+}
