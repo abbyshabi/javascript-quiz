08d5cc1eb53283140a4c483603253b952e6f4eff
abby-shabi <abbyshabi@gmail.com>
2018-12-10 15:08:55 +0300

    modified my js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 function computeQuiz() {
-	var Q1 = document.getElementsByName('question1');
-	var Q2 = document.getElementsByName('question2');
-	var Q3 = document.getElementsByName('question3');
-	var Q4 = document.getElementsByName('question4');
+	var qOne = document.getElementsByName('question1');
+	var qTwo = document.getElementsByName('question2');
+	var qThree = document.getElementsByName('question3');
+	var qFour = document.getElementsByName('question4');
 	var score = 0;
 
-	for (var i = 0, length = Q1.length; i < length; i++) {
-		if (Q1[i].checked) {
-			var ans1 = Q1[i].value
-			if (Q1[i].value === 'd') {
+	for (var i = 0, length = qOne.length; i < length; i++) {
+		if (qOne[i].checked) {
+			var ans1 = qOne[i].value
+			if (qOne[i].value === 'd') {
 				score +=25
 			}
 		}
 	}
-	for (var i = 0, length = Q2.length; i < length; i++) {
-		if (Q2[i].checked) {
-			var ans2 = Q2[i].value
-			if (Q2[i].value === 'c') {
+	for (var i = 0, length = qTwo.length; i < length; i++) {
+		if (qTwo[i].checked) {
+			var ans2 = qTwo[i].value
+			if (qTwo[i].value === 'c') {
 				score +=25
 			}
 		}
 	}
-	for (var i = 0, length = Q3.length; i < length; i++) {
-		if (Q3[i].checked) {
-			var ans3 = Q3[i].value;
-			if (Q3[i].value === 'c') {
+	for (var i = 0, length = qThree.length; i < length; i++) {
+		if (qThree[i].checked) {
+			var ans3 = qThree[i].value;
+			if (qThree[i].value === 'c') {
 				score +=25
 			}
 		}
 	}
-	for (var i = 0, length = Q4.length; i < length; i++) {
-		if (Q4[i].checked) {
-			var ans4 = Q4[i].value
-			if (Q4[i].value === 'b') {
+	for (var i = 0, length = qFour.length; i < length; i++) {
+		if (qFour[i].checked) {
+			var ans4 = qFour[i].value
+			if (qFour[i].value === 'b') {
 				score +=25
 			}
 		}
